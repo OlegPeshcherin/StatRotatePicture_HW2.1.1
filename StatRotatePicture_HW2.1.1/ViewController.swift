9f34e6e559728a04ee290e24a108aa0388b109d1
OlegPeshcherin <olegpnmsk@me.com>
2021-10-21 08:50:42 +0300

    Initial Commit

---
+++ StatRotatePicture_HW2.1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StatRotatePicture_HW2.1.1
+//
+//  Created by Oleg Peshcherin on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
